76ec45beb046620f97045cec48d00be21dcba190
TheSh4h <hh.haseebshah@gmail.com>
2023-06-03 09:25:13 -0700

    header complete

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,11 @@
 import "./Header.css";
 import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from "./HeaderOption";
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 const Header = () => {
     return ( 
@@ -14,6 +20,12 @@ const Header = () => {
             </div>
 
             <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Home" />
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+                <HeaderOption Icon={ChatIcon} title="Chat" />
+                <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+                <HeaderOption avatar="img/zoro.jpg" title="me" />
 
             </div>
         </div>
