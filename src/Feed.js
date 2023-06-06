f8ec7f049bccf81ba102e4004b72d99afb285e1a
TheSh4h <hh.haseebshah@gmail.com>
2023-06-06 09:34:29 -0700

    feed input done

--- src/Feed.js
+++ src/Feed.js
@@ -1,9 +1,18 @@
 import "./Feed.css";
+import CreateIcon  from "@mui/icons-material/Create";
 
 const Feed = () => {
     return ( 
         <div className="feed">
-
+            <div className="feed__inputContainer">
+                <div className="feed__input">
+                    <CreateIcon />
+                    <form>
+                        <input type="text" />
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
+            </div>
         </div>
      );
 }
