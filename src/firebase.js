a6d5913881ec9d4b1007d30db14ee4c16232a922
TheSh4h <hh.haseebshah@gmail.com>
2023-07-04 06:27:41 -0700

    firebase installed

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyC8k9JaB6hBRXK5N-zkm6R-MqwSCxmhhCE",
+  
+    authDomain: "linkedin-clone-3c66b.firebaseapp.com",
+  
+    projectId: "linkedin-clone-3c66b",
+  
+    storageBucket: "linkedin-clone-3c66b.appspot.com",
+  
+    messagingSenderId: "593142787774",
+  
+    appId: "1:593142787774:web:fedef630824ed70fabb8ef"
+  
+  };
